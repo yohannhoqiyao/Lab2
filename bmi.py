a110428f793be72e0fbd944b4db8dd932c2e0452
Yohann Ho <yohannho.23@ichat.sp.edu.sg>
2024-04-30 13:37:19 +0800

    dog

--- bmi.py
+++ bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height,weight):
     print("BMI = ", bmi)
     if bmi < 18.5:
         print("You are under weight")
+        return(-1)
     elif bmi <= 25:
         print("You are normal weight")
+        return(0)
     else:
         print("You are over weight")
+        return(1)
 
 calculate_bmi(weight=57,height=1.73)
 
